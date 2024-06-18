214fee6492b34015bf4ddb006efcff284d7358dd
siascone <spencer.iascone@gmail.com>
2024-06-18 09:59:26 -0600

    setup redux store and provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Provider } from 'react-redux';
 import './index.css';
+import configureStore from './store/store.js';
+
+const store = configureStore();
+
+if (import.meta.env.MODE !== 'production') {
+    window.store = store;
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
   </React.StrictMode>
 );
+
